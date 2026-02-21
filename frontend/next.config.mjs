49721def75a702fe74723d31adb3e56df67ab0bf
gaddamvivek <gaddamvivek01@gmail.com>
2026-02-20 20:23:44 -0500

    version upgrade

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'planvoyage-destinations.s3.us-east-2.amazonaws.com',
-            pathname: '/**',
-          },
-        ],
+        remotePatterns: [],
       },
 };
 
